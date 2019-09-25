4c5da1fef0d0e9377a6218b598e1ed917e9dd4af
tharangni <tharangni@hotmail.com>
2019-09-25 17:57:08 +0200

    Saving json response to local file. 1. Need to structure the json 2. Apply for infinite loop

--- script.py
+++ script.py
@@ -1,9 +1,30 @@
+import re
 import json
+import scrapy
 import requests
 
 import pandas as pd
 
 from lxml import html
+from datetime import datetime
+
+
+
+def saveJSON(path, data):
+
+	# writes json data to file path
+
+	with open(path, 'w') as file:
+		json.dump(data, file)
+
+
+def parseJSON(json_file):
+	
+	# @todo: read json and extract only relevant fields
+
+	read_file = pd.read_json(json_file, lines=True)
+	return read_file
+
 
 class MediumScraper:
 	'''
@@ -29,15 +50,26 @@ class MediumScraper:
 		'''
 		return('https://medium.com/@'+self.username+'/')
 
+
+	def getClapsURL(self):
+		'''
+		Returns the url for claps
+
+		'''
+		return self.getUserProfile()+'has-recommended'
+		# return 'https://www.medium.com/search/posts?q=Data%20Science'
+
+
 	def getRequest(self):
 		'''
 		Gets the `claps` of the user profile
 		'''
-		URL = 'https://medium.com/@'+self.username+'/has-recommended'
+		URL = self.getClapsURL()
 		medium_recommend = requests.get(URL)
 		tree = html.fromstring(medium_recommend.content)
 
 		return tree
+
 
 	def parseHTML(self, tree):
 		'''
@@ -54,22 +86,55 @@ class MediumScraper:
 
 		return userJson, clapsJson
 
-def parseJSON(json_file):
-	
-	# @todo: read json and extract only relevant fields
 
-	read_file = pd.read_json(json_file, lines=True)
-	return read_file
+class ClapsScraper(scrapy.Spider):
+	'''
+	docstring for ClapsScraper
+	'''
+	name = "claps_scraper"
+	handle_http_list = [400, 401]
+	auto_throttle_enable = True
+	
+
+	def start_requests(self):
+
+		user = MediumScraper('tharangni')
+		
+		start_urls = [user.getClapsURL()]
+
+		# cookie = "__cfduid=d13dc96d7996d6bf5168dc50cf43610881546335152; uid=1e473cbcde46; sid=1:D2btGU5i9eacrLuBlsVoMMEQvrl8QBPMuE5hxBDfHyQDXw0bxZfnthWzo9/4jBqq; lightstep_guid/lite-web=4bb788422492a14e; lightstep_session_id=33a3ad460d691fcb; pr=1; tz=-120; sz=1351; xsrf=3std0BPdCjMD; __cfruid=59e31e646a3b110fdde50a4b3e2efffd626fbe49-1569357959"
+		# header = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36"
+
+		for url in start_urls:
+			yield scrapy.Request(url, method='GET', callback=self.parse)
+
+	def parse(self, response):
+
+		response_data = response.text
+		response_split = re.findall(r'<script>(.*?)</script>', response_data)
+		response_data = response_split[3:]
+
+		dt_string = datetime.now().strftime("%d%m%Y_%H%M%S")
+		filename = "medium_{}_claps_{}.json".format('tharangni', dt_string)
+
+		saveJSON(filename, response_data[-2].split("window.__APOLLO_STATE__ =")[-1])
+
+		df = parseJSON(filename)
+		
+		print(df.head(5))
+
+		pass		
 
 
 if __name__ == '__main__':
 
-	r = parseJSON("user.json")
-	print(r)
+	# r = parseJSON("user.json")
+	# print(r)
 
 	# main()
 
-	# obj = MediumScraper('tharangni')
+	obj = MediumScraper('tharangni')
+	print(obj.getClapsURL())
 	# a, b = obj.parseHTML(obj.getRequest())
 
 	# with open('user.json', 'w') as f:
